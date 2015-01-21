57c679b3bd6451c1e0f917dcab16e58afc53ab47
HKCHO <j880825@gmail.com>
2015-01-21 23:11:50 +0900

    BoardService.java

--- iumui/src/main/java/java63/iumui/service/BoardService.java
+++ iumui/src/main/java/java63/iumui/service/BoardService.java
@@ -2,6 +2,7 @@ package java63.iumui.service;
 
 import java.util.HashMap;
 import java.util.List;
+
 import java63.iumui.dao.BoardDao;
 import java63.iumui.domain.Board;
 import java63.iumui.domain.BoardComment;
@@ -10,11 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-/* Service 컴포넌트의 역할
- * => 비즈니스 로직 수행
- * => 트랜잭션 관리
- */
 
 @Service
 public class BoardService {
@@ -25,7 +21,6 @@ public class BoardService {
    
     HashMap<String,Object> paramMap = new HashMap<>();
     paramMap.put("categoryNo", categoryNo);
-    //paramMap.put("startIndex", ((pageNo - 1) * pageSize));
     paramMap.put("pageNo", pageNo);
     paramMap.put("pageSize", pageSize);
     
@@ -45,20 +40,6 @@ public class BoardService {
     return boardDao.selectAllList();
   }
   
-  /*
-  public int getMaxPageNo(int pageSize) {
-    int totalSize = boardDao.totalSize();
-    int maxPageNo = totalSize / pageSize;
-    if ((totalSize % pageSize) > 0) maxPageNo++;
-    
-    return maxPageNo;
-  }
-  */
-  /* @Transactional 선언
-   * => 메서드 안의 입력/변경/삭제(manipluation) 작업을 하나의 작업을 묶는다.
-   * => 모든 작업이 성공했을 때만 서버에 반영한다. 
-   */
-  
   @Transactional(
       rollbackFor=Exception.class, 
       propagation=Propagation.REQUIRED)
@@ -66,9 +47,12 @@ public class BoardService {
     boardDao.insert(board); 
   }
   
+  @Transactional(
+  		rollbackFor=Exception.class,
+  		propagation=Propagation.REQUIRED)
   public Board get(int boardNo) {
     Board board = boardDao.selectOne(boardNo);
-    
+    boardDao.updateClick(boardNo);
     //board.setPhotoList( boardDao.selectPhoto(boardNo));
     
     return board;
@@ -85,6 +69,7 @@ public class BoardService {
   public void addComment(BoardComment boardComment) {
     boardDao.insertComment(boardComment); 
   }
+  
   @Transactional(
       rollbackFor=Exception.class, 
       propagation=Propagation.REQUIRED)
@@ -119,9 +104,30 @@ public class BoardService {
     
     boardDao.request(paramMap);
   }
+
+
+  @Transactional(
+  		rollbackFor=Exception.class, 
+  		propagation=Propagation.REQUIRED)
+  public void requestAccept(int boardNo, int memberNo) {
+  	HashMap<String,Object> paramMap = new HashMap<>();
+  	paramMap.put("boardNo", boardNo);
+  	paramMap.put("memberNo", memberNo);
+
+  	boardDao.requestAccept(paramMap);
+  }
+  
+  @Transactional(
+      rollbackFor=Exception.class, 
+      propagation=Propagation.REQUIRED)
+  public void requestReject(int boardNo, int memberNo) {
+    HashMap<String,Object> paramMap = new HashMap<>();
+    paramMap.put("boardNo", boardNo);
+    paramMap.put("memberNo", memberNo);
+    
+    boardDao.requestReject(paramMap);
+  }  
 }
-
-
 
 
 
