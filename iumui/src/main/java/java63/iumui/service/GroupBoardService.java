e8758bf179e1f54392ced12f0fcf27fe23fcbe73
HKCHO <j880825@gmail.com>
2015-01-31 16:43:07 +0900

    GroupBoardMembers.java

---
+++ iumui/src/main/java/java63/iumui/service/GroupBoardService.java
@@ -0,0 +1,71 @@
+package java63.iumui.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import java63.iumui.dao.GroupBoardDao;
+import java63.iumui.domain.GroupBoard;
+import java63.iumui.domain.GroupBoardComment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class GroupBoardService {
+  @Autowired GroupBoardDao groupBoardDao;
+  
+  @Transactional(
+      rollbackFor=Exception.class, 
+      propagation=Propagation.REQUIRED)
+  public List<?> getList(int groupNo) {
+    
+    return groupBoardDao.selectList(groupNo);
+  }
+  
+  public List<?> getComments(int groupNo) {
+    
+    return groupBoardDao.selectComments(groupNo);
+  }
+  
+  @Transactional(
+      rollbackFor=Exception.class, 
+      propagation=Propagation.REQUIRED)
+  public int getGroupMemberNo(int groupNo, int memberNo) {
+    HashMap<String,Object> paramMap = new HashMap<>();
+    paramMap.put("groupNo", groupNo);
+    paramMap.put("memberNo", memberNo);
+    
+    return groupBoardDao.selectGroupMemberNo(paramMap);
+  }
+  
+  @Transactional(
+      rollbackFor=Exception.class, 
+      propagation=Propagation.REQUIRED)
+  public void addGroupBoard(GroupBoard groupBoard) {
+    groupBoardDao.insertGroupBoard(groupBoard);
+  }
+  
+  @Transactional(
+      rollbackFor=Exception.class, 
+      propagation=Propagation.REQUIRED)
+  public void addGroupBoardComment(GroupBoardComment groupBoardComment) {
+    groupBoardDao.insertGroupBoardComment(groupBoardComment);
+  }
+  
+  @Transactional(
+      rollbackFor=Exception.class, 
+      propagation=Propagation.REQUIRED)
+  public void updateGroupBoard(GroupBoard groupBoard) {
+    groupBoardDao.updateGroupBoard(groupBoard);
+  }
+  
+  @Transactional(
+      rollbackFor=Exception.class, 
+      propagation=Propagation.REQUIRED)
+  public void delete(int groupBoardNo) {
+    groupBoardDao.deleteComments(groupBoardNo);
+    groupBoardDao.delete(groupBoardNo);
+  }
+}
